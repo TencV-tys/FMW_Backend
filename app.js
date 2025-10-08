72ee494574659679dee1282b6bda548081cb256e
Vincent <vincenttayros078@gmail.com>
2025-10-08 15:55:52 +0800

    good for now

--- app.js
+++ app.js
@@ -14,4 +14,4 @@ app.use(express.json());
 app.use('/api', authRoutes);
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
--- migrations/20251008064241_create_users.js
+++ migrations/20251008064241_create_users.js
@@ -10,9 +10,9 @@ exports.up = function(knex) {
     table.string('last_name').notNullable();
     table.string('email').notNullable().unique();
     table.string('password').notNullable();
+    table.string('gender').nullable();
     table.string('phone');
     table.enu('role', ['user', 'admin']).defaultTo('user');
-    table.enu('gender', ['male', 'female', 'other']);
     table.string('profile_photo');
     table.timestamps(true, true); // created_at & updated_at
   });
