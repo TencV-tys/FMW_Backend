e6dcab824cac35369ee5e349748d218a5146261f
Vincent <vincenttayros078@gmail.com>
2025-11-08 22:06:30 +0800

    added backend fixed

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express'); 
 const router = express.Router();
 const postController = require('../controllers/postController');
 const { authMiddleware } = require('../middleware/authMiddleware');
