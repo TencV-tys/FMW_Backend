01f81647d40c0ef94024525f02f9bd22c6985d3d
Vincent <vincenttayros078@gmail.com>
2025-11-11 19:36:50 +0800

    adedfixed backend complte

--- seeds/03_admin_user.js
+++ seeds/03_admin_user.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcrypt');
-
+require('dotenv').config();
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
@@ -7,17 +7,24 @@ const bcrypt = require('bcrypt');
 exports.seed = async function (knex) {
   // Deletes ALL existing admins
   await knex('users').where({ role: 'admin' }).del();
+ 
 
+   // 🆕 CHECK if env variables exist
+  if (!process.env.ADMIN_EMAIL || !process.env.ADMIN_PASS) {
+    throw new Error('ADMIN_EMAIL and ADMIN_PASS environment variables are required');
+  }
+
+  
   // Hash the password
-  const hashedPassword = await bcrypt.hash('admin9087', 10);
+  const hashedPassword = await bcrypt.hash(process.env.ADMIN_PASS, 10);
 
   // Insert default admin user
   await knex('users').insert([
     {
-      first_name: 'System',
+      first_name: 'Admin',
       last_name: 'Administrator',
-      email: 'afmw203@gmail.com',
-      password: hashedPassword,
+      email:process.env.ADMIN_EMAIL,
+      password:process.env.ADMIN_PASS,
       role: 'admin',
       gender: 'N/A',
       profile_photo: null,
